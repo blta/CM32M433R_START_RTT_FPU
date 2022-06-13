aa90f76018adbe3dc8f417a48978419a969d9460
blta <liutao_dz@163.com>
2022-06-13 20:25:09 +0800

    add ARCH_RISCV_FPU and ARCH_RISCV_FPU_S macros

--- application/cpu/risc-v/nuclei/cpuport.h
+++ application/cpu/risc-v/nuclei/cpuport.h
@@ -18,6 +18,35 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+/* bytes of register width  */
+#define ARCH_RISCV_FPU
+#define ARCH_RISCV_FPU_S
+
+#ifdef ARCH_CPU_64BIT
+#define STORE                   sd
+#define LOAD                    ld
+#define REGBYTES                8
+#else
+#define STORE                   sw
+#define LOAD                    lw
+#endif
+
+/* FPU */
+#ifdef ARCH_RISCV_FPU
+#ifdef ARCH_RISCV_FPU_D
+#define FSTORE                  fsd
+#define FLOAD                   fld
+#define FREGBYTES               8
+#define rv_floatreg_t           rt_int64_t
+#endif
+#ifdef ARCH_RISCV_FPU_S
+#define FSTORE                  fsw
+#define FLOAD                   flw
+#define FREGBYTES               4
+#define rv_floatreg_t           rt_int32_t
+#endif
+#endif
+
 
 /* Scheduler utilities. */
 #define RT_YIELD()                                                              \
